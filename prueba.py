5c81ac7163d847e6983f951fa32d08f3bb0fabb4
Jesus Bautista <97202824+JesusBHZ@users.noreply.github.com>
2022-04-06 16:55:41 +0000

    ssssss

--- prueba.py
+++ prueba.py
@@ -1,4 +1,4 @@
-class Sokoban:
+"""class Sokoban:
     mapa = []  # Mapa de juego
 
     def __init__(self):
@@ -6,3 +6,11 @@ class Sokoban:
 
     def moverDerecha(self):
         print("Mover")
+https://naps.com.mx/blog/impresion-de-cadenas-en-python/
+"""
+cadena = "¡Hola, mundo!"
+
+# Método 2, con índice
+for indice in range(len(cadena)):
+    caracter = cadena[indice]
+    print("En el índice {} tenemos a '{}'".format(indice, caracter))
